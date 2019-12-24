865935638d068edadde8ddfb671515ed58f2fed0
trunghc-0841 <huynh.chi.trung@sun-asterisk.com>
2019-12-24 20:08:31 +0700

    validate

--- client/app/models/attendance.rb
+++ client/app/models/attendance.rb
@@ -1,4 +1,6 @@
 class Attendance < ApplicationRecord
   belongs_to :lesson
   belongs_to :student, foreign_key: "student_code", primary_key: "code"
+
+  validates_uniqueness_of :student_code, scope: :lesson_id
 end
