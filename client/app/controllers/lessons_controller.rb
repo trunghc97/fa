9688475e6eefae9404c65755ebec6a73a81dd188
trunghc-0841 <huynh.chi.trung@sun-asterisk.com>
2020-01-07 22:03:30 +0700

    export excel

--- client/app/controllers/lessons_controller.rb
+++ client/app/controllers/lessons_controller.rb
@@ -9,6 +9,14 @@ class LessonsController < ApplicationController
 
   def show
     @lesson = Lesson.find params[:id]
-    @attendances = @lesson.attendances
+    @students = @lesson.course_class.students.map {|a| [a.code, a.name]}
+    attendances = @lesson.attendances.map {|a| [a.student_code, a.created_at.strftime("%H:%M")]}
+    @students.each_with_index do |student, index|
+      attendances.each do |attendance|
+        if student[0] == attendance[0]
+          @students[index] << attendance[1]
+        end
+      end
+    end
   end
 end
--- client/config/routes.rb
+++ client/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :course_classes
+  resources :course_classes do
+    get "export_excel"
+  end
   resources :lessons do
     resources :attendances, shallow: true
   end
